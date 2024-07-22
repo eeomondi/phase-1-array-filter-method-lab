a6412029444cc110af5ee500f60e9dd3b5b20635
Elvis <elvis.omondi@student.moringaschool.com>
2024-07-22 11:31:34 +0300

    commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Code your solution here
 
 function findMatching(drivers, query) {
-    return drivers.filter(driver => driver.name.toLowerCase().includes(query.toLowerCase()));
+    return drivers.filter(driver => driver.name && driver.name.toLowerCase().includes(query.toLowerCase()));
   }
   
   function fuzzyMatch(drivers, query) {
-    return drivers.filter(driver => driver.name.toLowerCase().startsWith(query.toLowerCase()));
+    return drivers.filter(driver => driver.name && driver.name.toLowerCase().startsWith(query.toLowerCase()));
   }
   
   function matchName(drivers, query) {
-    return drivers.filter(driver => driver.name === query);
+    return drivers.filter(driver => driver.name && driver.name === query);
   }
   
   export default { findMatching, fuzzyMatch, matchName };
